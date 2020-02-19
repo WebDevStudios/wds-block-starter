21af511751c639ab64ff79cc621332018c6ed427
Mike England <mike.england@webdevstudios.com>
2020-02-19 09:04:20 -0500

    Save state while working.
    - Updated composer files
    - Moved src & build directories to root
    - Modified custom build paths for package.json back to WP Script defaults
    - Imported various OopsWPDemo files to get basic features working
    - Removed all non block related build features
    - Updated gitignore

---
+++ oops-wp-demo.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: WDS Block Starter
+ * Plugin URI:  https://github.com/WebDevStudios/wds-block-starter
+ * Description:
+ * Version:     0.0.1
+ * Author:      Mike England
+ * License:     GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: ded
+ * Domain Path: /languages
+ */
+
+use WebDevStudios\OopsWPDemo\OopsWPDemo;
+
+$autoload = __DIR__ . '/vendor/autoload.php';
+
+if ( is_readable( $autoload ) ) {
+	require_once $autoload;
+}
+
+add_action( 'plugins_loaded', function() {
+	try {
+		( new OopsWPDemo( __FILE__ ) )->run();
+	} catch ( Error $e ) {
+		add_action( 'admin_notices', function() {
+			$message = __(
+				'Could not locate OOPS-WP Demo class files. Did you remember to run composer install?',
+				'oops-wp-demo'
+			);
+
+			echo wp_kses_post( '<div class="notice notice-error"><p>' . $message . '</p></div>' );
+		} );
+	}
+} );
---
+++ src/BlockEditor/BlockRegistrar.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Example service which registers a collection of custom WordPress editor blocks.
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-02-03
+ * @package WebDevStudios\OopsWPDemo\BlockEditor
+ */
+
+namespace WebDevStudios\OopsWPDemo\BlockEditor;
+
+use WebDevStudios\OopsWP\Structure\Editor\EditorBlockInterface;
+use WebDevStudios\OopsWP\Structure\Service;
+use WebDevStudios\OopsWP\Utility\FilePathDependent;
+use WebDevStudios\OopsWPDemo\BlockEditor\Block\Game;
+
+/**
+ * Class BlockRegistrar
+ *
+ * @author  Jeremy Ward <jeremy.ward@webdevstudios.com>
+ * @since   2020-02-03
+ * @package WebDevStudios\OopsWPDemo\BlockEditor
+ */
+class BlockRegistrar extends Service {
+	use FilePathDependent;
+
+	/**
+	 * Editor blocks registered by this service.
+	 *
+	 * @var array
+	 * @since 2020-02-03
+	 */
+	private $blocks = [
+		Game::class,
+	];
+
+	/**
+	 * Register this service with WordPress.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	public function register_hooks() {
+		add_action( 'init', [ $this, 'register_blocks' ] );
+	}
+
+	/**
+	 * Hook callback to register blocks with the editor.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	public function register_blocks() {
+		foreach ( $this->blocks as $block_class ) {
+			$block = new $block_class();
+			$this->register_block( $block );
+		}
+	}
+
+	/**
+	 * Register an editor block with WordPress.
+	 *
+	 * @param EditorBlockInterface $block An object instance.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2020-02-03
+	 */
+	private function register_block( EditorBlockInterface $block ) {
+		/*
+		 * @TODO Update this when EditorBlock is updated. `set_file_path` should be part of the interface.
+		 */
+		if ( in_array( FilePathDependent::class, class_uses( $block ), true ) ) {
+			$block->set_file_path( $this->file_path );
+		}
+
+		$block->register();
+	}
+}
