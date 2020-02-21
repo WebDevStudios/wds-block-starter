163f9bdc3bb1f7123faf5950d9a3e8fa60ecf505
Mike England <mike.england@webdevstudios.com>
2020-02-21 10:19:33 -0500

    Setup webpack config
    - Created basic config ont top of WP Script Defaults
    - Added new entry point for css.js in src
    - Setup splitChunks cacheGroups for style files to export
    - Setup loaders for MiniCssExtractPlugin & PostCSS
    - Add MiniCssExtractPlugin to plugins for file naming
    - Add CleanWebpackPlugin for directory wipe upon new build
    - Add IgnoreEmitPlugin to prevent unneccessary file generation

---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+const path = require('path');
+
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const IgnoreEmitPlugin = require( 'ignore-emit-webpack-plugin' );
+
+module.exports = {
+    ...defaultConfig,
+    entry: {
+        ...defaultConfig.entry,
+        css: path.resolve( process.cwd(), 'src', 'css.js' ),
+    },
+    optimization: {
+        namedChunks: true,
+        namedModules: true,
+        splitChunks: {
+            cacheGroups: {
+                styles: {
+                    name: 'style',
+                    test: /style\.(sa|sc|c)ss$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+                editor: {
+                    name: 'editor',
+                    test: /editor\.(sa|sc|c)ss$/,
+                    chunks: 'all',
+                    enforce: true,
+                },
+            },
+        },
+    },
+    module: {
+        ...defaultConfig.module,
+        rules: [
+            ...defaultConfig.module.rules,
+            {
+              test: /\.css$/,
+              use: [
+                {
+                    loader: MiniCssExtractPlugin.loader,
+                    options: {
+                        hmr: process.env.NODE_ENV === 'development',
+                    },
+                },
+                { loader: 'css-loader', options: { importLoaders: 1 } },
+                'postcss-loader'
+              ]
+            },
+        ],
+    },
+    plugins: [
+        ...defaultConfig.plugins,
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+             filename: '[name].css',
+             chunkFilename: '[id].css',
+        }),
+        new IgnoreEmitPlugin( [ 'index.asset.php', 'css.asset.php', 'css.js', 'css.js.map', 'style.js', 'editor.js' ] ),
+    ],
+};
