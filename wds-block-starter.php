462e848a62a9f6c61b89c8a545638e295ed452b8
Mike England <mike.england@webdevstudios.com>
2020-02-21 10:18:51 -0500

    Fix commit state for main plugin file

--- wds-block-starter.php
+++ wds-block-starter.php
@@ -1,4 +1,36 @@
-Unstaged changes after reset:
-D	oops-wp-demo.php
-M	src/index.js
-M	wds-block-starter.php
+<?php
+/**
+ * Plugin Name: WDS Block Starter
+ * Plugin URI:  https://github.com/WebDevStudios/wds-block-starter
+ * Description: A starter plugin for Gutenberg blocks development.
+ * Author: WebDevStudios
+ * Author URI: https://webdevstudios.com
+ * Version:     0.0.1
+ * License:     GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * Text Domain: ded
+ * Domain Path: /languages
+ */
+
+use WebDevStudios\OopsWPDemo\OopsWPDemo;
+
+$autoload = __DIR__ . '/vendor/autoload.php';
+
+if ( is_readable( $autoload ) ) {
+	require_once $autoload;
+}
+
+add_action( 'plugins_loaded', function() {
+	try {
+		( new OopsWPDemo( __FILE__ ) )->run();
+	} catch ( Error $e ) {
+		add_action( 'admin_notices', function() {
+			$message = __(
+				'Could not locate OOPS-WP Demo class files. Did you remember to run composer install?',
+				'oops-wp-demo'
+			);
+
+			echo wp_kses_post( '<div class="notice notice-error"><p>' . $message . '</p></div>' );
+		} );
+	}
+} );
