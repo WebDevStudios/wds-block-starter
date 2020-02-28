5c31ed44a3f18f88a17615f669953ee37cc88fc9
Mike England <mike.england@webdevstudios.com>
2020-02-28 11:03:14 -0500

    Move eslint standards to package.json
    - Removed old eslint files
    - Extended standards in package.json

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-// https://www.npmjs.com/package/@wordpress/eslint-plugin
-module.exports = {
-	"globals": { "wp": true },
-	"extends": [
-		"plugin:@wordpress/eslint-plugin/esnext",
-		"plugin:@wordpress/eslint-plugin/react",
-		"plugin:@wordpress/eslint-plugin/recommended"
-	]
-}
