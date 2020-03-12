bd42ffe814f7cf7777bbff9de02506a069c20ae8
Greg Rickaby <greg@gregrickaby.com>
2020-03-12 10:53:21 -0500

    remove oops-wp

--- wds-block-starter.php
+++ wds-block-starter.php
@@ -20,5 +20,44 @@ namespace WebDevStudios\BlockStarter;
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
 
-// Enqueue files.
-require plugin_dir_path( __FILE__ ) . '/inc/enqueue-scripts.php';
+/**
+ * Register the block with WordPress.
+ *
+ * @author WebDevStudios
+ * @since 0.0.1
+ */
+function register_block() {
+
+	// Register editor script.
+	wp_register_script(
+		'wdsbs-editor-script',
+		plugins_url( 'build/index.js', __FILE__ ),
+		[ 'wp-editor' ],
+		filemtime( plugin_dir_path( __FILE__ ) . 'build/index.js' ),
+		true
+	);
+
+	// Register editor style.
+	wp_register_style(
+		'wdsbs-editor-style',
+		plugins_url( 'build/editor.css', __FILE__ ),
+		[ 'wp-edit-blocks' ],
+		filemtime( plugin_dir_path( __FILE__ ) . 'build/editor.css' )
+	);
+
+	// Register frontend style.
+	wp_register_style(
+		'wdsbs-style',
+		plugins_url( 'build/style.css', __FILE__ ),
+		[],
+		filemtime( plugin_dir_path( __FILE__ ) . 'build/style.css' )
+	);
+
+	// Register block with WordPress.
+	register_block_type( 'wdsbs/rich-text-demo', array(
+		'editor_script' => 'wdsbs-editor-script',
+		'editor_style'  => 'wdsbs-editor-style',
+		'style'         => 'wdsbs-style',
+	) );
+}
+add_action( 'init', 'WebDevStudios\BlockStarter\register_block' );
