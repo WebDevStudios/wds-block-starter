fd56b354b84193905ac675eb81aadee1d7710680
Mike England <mike.england@webdevstudios.com>
2020-03-12 14:09:47 -0400

    Update cssnano on production only
    - Create basic plugins array
    - Conditionally add plugins based on production variable
    
    See https://github.com/WebDevStudios/wds-block-starter/issues/9

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,21 @@
-module.exports = {
-	plugins: [
-		require( 'precss' ),
-		require( 'autoprefixer' ),
+const production = 'production' === process.env.NODE_ENV ? true : false;
+
+// Assign plugins.
+const plugins = [
+	require( 'precss' ),
+	require( 'autoprefixer' ),
+];
+
+// Minify production.
+if ( production ) {
+	plugins.push(
 		require( 'cssnano' )( {
 			preset: 'default',
 		} ),
-	],
+	);
+}
+
+// Module Export
+module.exports = {
+	plugins,
 };
