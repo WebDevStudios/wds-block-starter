6cf675fdfc245a97e6fb7bfe1368f302e3d7163d
Mike England <mike.england@webdevstudios.com>
2020-07-17 17:33:42 -0400

    bump version to 1.0.0 and update README

--- wds-block-starter.php
+++ wds-block-starter.php
@@ -5,7 +5,7 @@
  * Description: A block starter for WebDevStudios projects.
  * Author: WebDevStudios
  * Author URI: https://webdevstudios.com
- * Version:     0.0.1
+ * Version:     1.0.0
  * License:     GPLv3
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain: wdsbs
