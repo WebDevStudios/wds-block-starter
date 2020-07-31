d4b6d67f3eb706572054b980b3e187239f3c7b19
Sal Ferrarello <sal.ferrarello@webdevstudios.com>
2020-07-31 14:52:21 -0400

    Add Jest test for custom Save component

--- src/block/rich-text/__tests__/save.js
+++ src/block/rich-text/__tests__/save.js
@@ -10,6 +10,8 @@ import { create as createTestRenderer } from 'react-test-renderer';
  */
 import { RichText } from '@wordpress/block-editor';
 
+import Save from 'src/block/rich-text/save';
+
 test(
 	'adds 1 + 2 to equal 3',
 	() => {
@@ -22,6 +24,20 @@ test(
 // @see https://jestjs.io/docs/en/tutorial-react
 test( 'renders RichText from WordPress components', () => {
 	const testRenderer = createTestRenderer( <RichText.Content value="hi" /> );
+
+	expect( testRenderer.toJSON() ).toMatchSnapshot();
+} );
+
+test( 'renders our Save component', () => {
+	const attributes = {
+		content: 'My content',
+	};
+	const testRenderer = createTestRenderer(
+		<Save
+			attributes={attributes}
+			className="my-class"
+		/>
+	);
 
 	expect( testRenderer.toJSON() ).toMatchSnapshot();
 } );
